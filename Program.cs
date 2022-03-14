26371a6f44e7542bc71a5d73f9f15b707e4eb1f3
ErickFPCruz <erick.cruz97@hotmail.com>
2022-03-13 23:39:54 -0300

    Termina a segunda parte do trabalho

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Alok & Bhaskar - Fuego");
+Console.WriteLine();
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
 Console.WriteLine("Soy el fuego que");
+Console.ResetColor();
